5c456a77f68398e7db32befce572612faa3fd973
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2022-12-05 15:09:46 +0300

    Переход на #5 задание
    Надо настроить Make последнее что сделал это brain-even. Надо добавить запись терминала в README.md https://asciinema.org/

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ brain-games:
 	node bin/brain-games.js
 lint:	
 	npx eslint .
+brain-even:
+	node bin/brain-even.js	
--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,8 @@
 import readlineSync from 'readline-sync';
 
-function randomCount(min, max) {
-  return Math.floor(Math.random());
-}
-
-export default function Main() {
+ export default function Main() {
+  console.log('brain-games');
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello!, ${name}`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const number = randomCount(1, 100);
-  let trueAnswers = 0;
-  while(trueAnswers <= 2) {
-    console.log(`Question: ${number}`);
-    const ans = readlineSync.question('Your answer: ');
-  }
-
- 
-}
+ }
